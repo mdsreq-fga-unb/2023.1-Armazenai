f5a1a49533355867c39de4b2e8becc5e1e67e2c4
Kaio Enzo <kaioenzobr@gmail.com>
2023-06-10 16:02:33 -0300

    route: atualiza middleware para rotas em pt

--- armazenai-front_end/middleware.ts
+++ armazenai-front_end/middleware.ts
@@ -13,9 +13,9 @@ export async function middleware(req: NextRequest) {
 
   console.log(user)
 
-  // if user is signed in and the current path is / redirect the user to /account
+  // if user is signed in and the current path is / redirect the user to /cadastro
   if (user && req.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/account', req.url))
+    return NextResponse.redirect(new URL('/cadastro', req.url))
   }
 
   // if user is not signed in and the current path is not / redirect the user to /
@@ -27,5 +27,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/account', "/*"],
+  matcher: ['/', '/cadastro', "/*"],
 }
