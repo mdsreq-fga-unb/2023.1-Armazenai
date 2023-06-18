a160c4474ceaf2eac4f2373d8f1590d3e9b8dd28
Kaio Enzo <kaioenzobr@gmail.com>
2023-06-18 12:53:40 -0300

    fix: perfil info tipo do state de usuario
    Mudanças no form de usuário fizeram com que o tipo necessário do state seja `Usuario | null | undefined`

--- armazenai-front_end/src/app/perfil/page.tsx
+++ armazenai-front_end/src/app/perfil/page.tsx
@@ -9,7 +9,7 @@ import BasePage from "../components/navbar/basePage";
 import PerfilInfo from "./perfil-info";
 
 export default function Perfil() {
-  const [user, setUser] = useState<Usuario | null>(null);
+  const [user, setUser] = useState<Usuario | null | undefined>(null);
   const supabase = createClientComponentClient<Database>();
 
   useEffect(() => {
