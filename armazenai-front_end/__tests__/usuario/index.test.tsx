70251a661279ef08552f1b125775c3bce8ba771f
kaio.salgado <kaio.salgado@placeti.com.br>
2023-07-11 11:20:00 -0300

    feat: testes de integração de usuário

--- armazenai-front_end/__tests__/usuario/index.test.tsx
+++ armazenai-front_end/__tests__/usuario/index.test.tsx
@@ -8,6 +8,8 @@ import UsuarioForm, {
 } from "../../src/app/components/formulario/usuarioFormCadastro";
 import userEvent from "@testing-library/user-event";
 import { errosFormularioMensagem } from "../../src/app/helpers/validator/mensagensDeErro";
+import axios, { AxiosError } from "axios";
+import { ApiErro } from "../cliente/index.test";
 
 describe("Teste do formulário de criação de usuário", () => {
   it("deveria mostrar os erros do formulário de criação de usuário", async () => {
@@ -45,7 +47,7 @@ describe("Teste do formulário de criação de usuário", () => {
     });
   });
 
-  it("deveria mostrar o erro de email inválido", async () => {
+  it("deveria mostrar o erro de email e telefone inválido", async () => {
     const user = userEvent.setup();
     const mockOnSubmit = jest.fn();
     render(
@@ -76,8 +78,110 @@ describe("Teste do formulário de criação de usuário", () => {
     expect(mockOnSubmit).toHaveBeenCalledTimes(0);
     await waitFor(() => {
       expect(
-        new RegExp(errosFormularioMensagem.emailInvalido, "i")
+        screen.queryByText(/O telefone informado é inválido!/i)
       ).toBeInTheDocument();
+    });
+    // expect(
+    //   screen.queryByText(
+    //     new RegExp(errosFormularioMensagem.telefoneInvalido, "i")
+    //   )
+    // ).toBeInTheDocument();
+  });
+
+  describe("Teste de integração com a API de Cliente no supabase", () => {
+    let token: string | undefined;
+    let idToDelete: number | undefined;
+    let idUsuario: string | undefined;
+
+    beforeAll(() => {
+      axios.interceptors.request.use((config) => {
+        config.headers["apikey"] =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5bmlpd2R2c2Zwc2JleHJrb25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUwMjU3ODEsImV4cCI6MjAwMDYwMTc4MX0.YYdZlKJKT4Njbfg0k-a7YEg890f7ulZpCNqndDDf9Uk";
+        config.headers["Content-Type"] = "application/json";
+        config.baseURL = "https://lyniiwdvsfpsbexrkons.supabase.co/";
+        return config;
+      });
+    });
+
+    beforeEach(async () => {
+      await axios
+        .post("auth/v1/token?grant_type=password", {
+          email: "kaioenzobr@gmail.com",
+          password: "123321",
+        })
+        .then((d) => {
+          token = d.data.access_token;
+          idUsuario = d.data.user.id;
+        });
+      axios.interceptors.request.use((config) => {
+        config.headers["Authorization"] = `Bearer ${token}`;
+        return config;
+      });
+    });
+
+    afterEach(async () => {
+      if (idToDelete) {
+        const retorno = await axios.delete(
+          `rest/v1/profiles?id=eq.${idToDelete}`
+        );
+        expect(retorno.status).toBe(204);
+        idToDelete = undefined;
+      }
+      setTimeout(() => {}, 1000);
+    });
+
+    it("deveria retornar um usuário existente", async () => {
+      const data = await axios.get<Usuario[]>(
+        `rest/v1/profiles?select=*&id=eq.${idUsuario}`
+      );
+      expect(data.status).toBe(200);
+      expect(data.data.length).toBe(1);
+      expect(data.data[0].id).toBe(idUsuario);
+    });
+
+    it("deveria criar e retornar um usuário", async () => {
+      const data = await axios.post<UsuarioSignUp>(
+        "auth/v1/signup",
+        {
+          email: "teste@teste.teste",
+          password: "123321",
+        },
+        {
+          headers: {
+            Prefer: "return=representation",
+          },
+        }
+      );
+
+      expect(data.status).toBe(200);
+      expect(data.data.id).toBeDefined();
+      idToDelete = parseInt(data?.data.id);
+    });
+
+    it("deveria atualizar um cliente existente", async () => {
+      const data = await axios.patch<Usuario[]>(
+        `rest/v1/profiles?id=eq.${idUsuario}`,
+        {
+          nome: "Kaio Enzo",
+          cpf: "12345678910",
+          telefone: "12345678910",
+        },
+        {
+          headers: {
+            Prefer: "return=representation",
+          },
+        }
+      );
+
+      expect(data.status).toBe(200);
+      expect(data.data[0].nome).toBe("Kaio Enzo");
+      expect(data.data[0].cpf).toBe("12345678910");
+      expect(data.data[0].telefone).toBe("12345678910");
     });
   });
 });
+
+type UsuarioSignUp = {
+  id: string;
+  email: string;
+};
